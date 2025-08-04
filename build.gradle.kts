41536e99a3474475be9cef248efe28f570076fd1
Tert0 <tert0byte@gmail.com>
2025-08-04 18:05:22 +0200

    add gha workflow to publish package

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,10 @@ plugins {
     `java-library`
 
     `maven-publish`
-    signing
-    alias(libs.plugins.nmcp)
 }
 
 group = "dev.hollowcube"
-version = System.getenv("TAG_VERSION") ?: "dev"
+version = System.getenv("VERSION") ?: "dev"
 description = "Schematic reader and writer for Minestom"
 
 repositories {
@@ -35,68 +33,28 @@ tasks.test {
     useJUnitPlatform()
 }
 
-nmcpAggregation {
-    centralPortal {
-        username = System.getenv("SONATYPE_USERNAME")
-        password = System.getenv("SONATYPE_PASSWORD")
-        publishingType = if ("dev" in project.version.toString()) "USER_MANAGED" else "AUTOMATIC"
-    }
-
-    publishAllProjectsProbablyBreakingProjectIsolation()
-}
-
-publishing.publications.create<MavenPublication>("maven") {
-    groupId = "dev.hollowcube"
-    artifactId = "schem"
-    version = project.version.toString()
-
-    from(project.components["java"])
-
-    pom {
-        name.set(artifactId)
-        description.set(project.description)
-        url.set("https://github.com/hollow-cube/schem")
-
-        licenses {
-            license {
-                name.set("MIT")
-                url.set("https://github.com/hollow-cube/schem/blob/main/LICENSE")
+publishing {
+    repositories {
+        maven {
+            name = "allycraft-repository"
+            url = uri("https://mvn.allycraft.de/releases")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
             }
         }
+    }
 
-        developers {
-            developer {
-                id.set("mworzala")
-                name.set("Matt Worzala")
-                email.set("matt@hollowcube.dev")
-            }
-        }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.hollowcube"
+            artifactId = "schem"
+            version = project.version.toString()
 
-        issueManagement {
-            system.set("GitHub")
-            url.set("https://github.com/hollow-cube/schem/issues")
-        }
-
-        scm {
-            connection.set("scm:git:git://github.com/hollow-cube/schem.git")
-            developerConnection.set("scm:git:git@github.com:hollow-cube/schem.git")
-            url.set("https://github.com/hollow-cube/schem")
-            tag.set(System.getenv("TAG_VERSION") ?: "HEAD")
-        }
-
-        ciManagement {
-            system.set("Github Actions")
-            url.set("https://github.com/hollow-cube/schem/actions")
+            from(project.components["java"])
         }
     }
-}
-
-signing {
-    isRequired = System.getenv("CI") != null
-
-    val privateKey = System.getenv("GPG_PRIVATE_KEY")
-    val keyPassphrase = System.getenv()["GPG_PASSPHRASE"]
-    useInMemoryPgpKeys(privateKey, keyPassphrase)
-
-    sign(publishing.publications)
 }
